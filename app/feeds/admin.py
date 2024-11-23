04e61a85b8b7a1d31b6d8d847d5db2b01a6fb8ac
Marcus Crane <marcus@utf9k.net>
2024-11-23 17:28:54 +1300

    Add filtering options for feed entries
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- app/feeds/admin.py
+++ app/feeds/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Feed, FeedEntry
 
+@admin.register(FeedEntry)
+class FeedEntryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'feed_name', 'published_at', 'in_feed', 'backfilled']
+    list_filter = ["feed__title", "in_feed", "backfilled"]
+    date_hierarchy = "published_at"
+    ordering = ["published_at"]
+
+    def feed_name(self, obj):
+        return obj.feed.title
+
 admin.site.register(Feed)
-admin.site.register(FeedEntry)
