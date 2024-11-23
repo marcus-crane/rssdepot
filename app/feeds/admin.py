30556a2f4e38ab9efbf8c1b75342fbe44548048d
Marcus Crane <marcus@utf9k.net>
2024-11-23 17:30:31 +1300

    Add feed page filter
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- app/feeds/admin.py
+++ app/feeds/admin.py
@@ -5,11 +5,15 @@ from .models import Feed, FeedEntry
 @admin.register(FeedEntry)
 class FeedEntryAdmin(admin.ModelAdmin):
     list_display = ['title', 'feed_name', 'published_at', 'in_feed', 'backfilled']
-    list_filter = ["feed__title", "in_feed", "backfilled"]
-    date_hierarchy = "published_at"
-    ordering = ["published_at"]
+    list_filter = ['feed__title', 'in_feed', 'backfilled']
+    date_hierarchy = 'published_at'
+    ordering = ['published_at']
 
     def feed_name(self, obj):
         return obj.feed.title
 
-admin.site.register(Feed)
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ['title', 'hostname', 'backfilled']
+    list_filter = ['backfilled']
+    ordering = ['title']
