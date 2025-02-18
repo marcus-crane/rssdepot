83785c15d69dfb56010e18bd71f272a6455e69aa
Marcus Crane <marcus@utf9k.net>
2025-02-19 00:46:54 +1300

    Remove existing app
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- app/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python manage.py migrate
-python manage.py collectstatic --no-input --clear
-
-exec "$@"
--- app/feeds/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Nothing to see here yet.")
