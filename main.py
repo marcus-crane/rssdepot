6b24382c767038ddb1acd0de7d96fa09ea4a3de6
Marcus Crane <marcus@utf9k.net>
2025-02-19 21:43:38 +1300

    Add new feed
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import requests
 
 app = FastAPI()
 
+# We fetch some article sources to figure more info but we don't want to fetch for every run
+url_time_cache = {}
+
 @app.get("/")
 def root():
     return "Some useful feeds can be found here"
 
-@app.get("/hn_highlights.rss")
+@app.get("/hackernews-highlights.rss")
 def serve_highlights():
     r = requests.get("https://news.ycombinator.com/highlights")
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -48,6 +51,78 @@ def serve_highlights():
         fe.pubDate(pubDate=comment['date'])
         fe.link(href=comment['link'])
         fe.description(description=str(comment['text']), isSummary=False)
+
+    rssfeed = fg.rss_str(pretty=True)
+    return Response(content=rssfeed, media_type="application/xml")
+
+@app.get("/rnz-phil-pennington.rss")
+def serve_rnzpp():
+    r = requests.get("https://www.rnz.co.nz/authors/phil-pennington")
+    soup = BeautifulSoup(r.text, 'html.parser')
+    raw_articles = soup.find_all("div", class_="o-digest--news")
+
+    fg = FeedGenerator()
+    fg.title("RNZ - Phil Pennington")
+    fg.link(href="https://www.rnz.co.nz/authors/phil-pennington")
+    fg.description("Articles from Phil Pennington")
+
+    articles = []
+
+    for article in raw_articles:
+        text = article.find('div', class_='o-digest__summary').text
+        date_unparsed = article.find('span', class_='o-kicker__time').text
+        link_segment = article.find('h3', class_='o-digest__headline').a.attrs['href']
+        link = f"https://www.rnz.co.nz{link_segment}"
+        title = article.find('h3', class_='o-digest__headline').text
+
+        # if 'today' not in date_unparsed:
+        #     
+        # else:
+        if 'today' in date_unparsed:
+            today = pendulum.today(tz='Pacific/Auckland')
+            date_unparsed_tweaked = date_unparsed.replace(" today", "").replace("am", "AM").replace("pm", "PM")
+            parsed_time = pendulum.from_format(date_unparsed_tweaked, "h:mm A", tz='Pacific/Auckland')
+            date = today.set(hour=parsed_time.hour, minute=parsed_time.minute, second=0)
+
+        if 'today' not in date_unparsed:
+            # 2:14 pm on 18 February 2025
+            cached_date = url_time_cache.get(link, False)
+            if not cached_date:
+                # fetch page to get timestamp and cache result
+                r = requests.get(link)
+                soup2 = BeautifulSoup(r.text, 'html.parser')
+                # One article had a case of double whitespace one time
+                # https://www.rnz.co.nz/news/political/541208/trump-gaza-plan-not-proposal-but-threat-says-federation-of-islamic-associations
+                raw_date = soup2.find('span', class_='updated').text.strip().replace("am", "AM").replace("pm", "PM").replace("  ", " ")
+                print(raw_date, link)
+                date = pendulum.from_format(raw_date, "h:mm A [on] D MMMM YYYY", tz="Pacific/Auckland")
+                url_time_cache[link] = date
+            else:
+                date = cached_date
+        
+        articles.append({
+            'title': title,
+            'text': text,
+            'date': date,
+            'link': link
+        })
+    
+    articles.sort(key=lambda x: x['date'])
+
+    for article in articles:
+        fe = fg.add_entry()
+        fe.id(article['link'])
+        fe.title(article['title'])
+        fe.pubDate(pubDate=article['date'])
+        fe.link(href=article['link'])
+        fe.description(description=str(article['text']), isSummary=False)
+    
+    # clear cache with articles that have dropped out of feed
+    links = set([article['link'] for article in articles])
+    cached_links = set(url_time_cache.keys())
+    links_to_remove = cached_links - links
+    for link in links_to_remove:
+        del url_time_cache[link]
 
     rssfeed = fg.rss_str(pretty=True)
     return Response(content=rssfeed, media_type="application/xml")
